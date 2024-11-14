c3a5db5dbe6e96eebfc11be207815415de42998b
mohabbaticicle <mohabbatali@iciclecorporation.com>
2024-11-14 21:49:39 +0600

    udpdate and recheck project

--- src/app/utils/validateRequests.ts
+++ src/app/utils/validateRequests.ts
@@ -2,29 +2,28 @@ import { Request, Response, NextFunction } from "express";
 import { AnyZodObject, ZodOptional, ZodError } from "zod";
 
 const validateRequest = (schema: AnyZodObject | ZodOptional<AnyZodObject>) => {
-    return async (
-      req: Request,
-      res: Response,
-      next: NextFunction
-    ): Promise<void> => {
-      try {
-        // Only pass req.body instead of { body: req.body }
-        await schema.parseAsync(req.body);
-        next();
-      } catch (error) {
-        if (error instanceof ZodError) {
-          res.status(400).json({
-            success: false,
-            errors: error.errors.map((err) => ({
-              path: err.path,
-              message: err.message,
-            })),
-          });
-        } else {
-          next(error);
-        }
+  return async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> => {
+    try {
+      await schema.parseAsync(req.body);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        res.status(400).json({
+          success: false,
+          errors: error.errors.map((err) => ({
+            path: err.path,
+            message: err.message,
+          })),
+        });
+      } else {
+        next(error);
       }
-    };
+    }
   };
+};
 
 export default validateRequest;
